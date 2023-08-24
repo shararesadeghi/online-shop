6af69bf74c793c190007f226cba0edd7571c2246
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-08-24 15:58:28 +0430

    update react-router-dom

--- src/components/ProductDetails.js
+++ src/components/ProductDetails.js
@@ -1,5 +1,5 @@
 import React,{useContext} from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 //context
 import { ProductsContext } from '../context/ProductContextProvider';
 //css
@@ -7,8 +7,10 @@ import styles from '../components/ProductDetails.module.css';
 
 
 const ProductDetails = (props) => {
-
-    const id = props.match.params.id;
+     
+    const params = useParams();
+    const id = params.id;
+   // const id = props.match.params.id;
     const data = useContext(ProductsContext);
     const product = data[id - 1];
     const {image,title,description, price,category} = product;
--- src/components/shared/Product.js
+++ src/components/shared/Product.js
@@ -22,7 +22,7 @@ const Product = ({productData}) => {
                <div className = {styles.buttonContainer}>
                {quantityCount(state, id) ===1 && <button className = {styles.smallButton} onClick = {() => dispatch({type:"REMOVE_ITEM", payload: productData})}><img src = {trashIcon} alt = "trash"/></button>}
                {quantityCount(state, id) > 1 && <button className = {styles.smallButton} onClick = {() => dispatch({type:"DECREASE", payload: productData})}>-</button>}
-
+               {quantityCount(state, id) > 0 && <span className = {styles.counter}>{quantityCount(state,id)}</span> }
                {
                isIncCart(state, id) ? 
                <button className = {styles.smallButton} onClick = {() => dispatch({type: "INCREASE", payload: productData})}>+</button>:
